53fb238fc5fc3c160497ee37efac0b6706e955fb
zerogalaxy <zerogalaxy@protonmail.com>
2021-12-10 23:54:57 +0800

    first update

---
+++ precache-manifest.13dfa5cc178e3dc2be1585a483062c85.js
@@ -0,0 +1,190 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82875b9691081b3453a2",
+    "url": "/css/againJoin.8f396922.css"
+  },
+  {
+    "revision": "e88b5d6e3fc1103b3a88",
+    "url": "/css/app.88c6fc19.css"
+  },
+  {
+    "revision": "9c03a7f3e9644dd40f10",
+    "url": "/css/chunk-2a69d8cb.56b384b3.css"
+  },
+  {
+    "revision": "c76a1610e4b450b2e2ae",
+    "url": "/css/chunk-54e934c9.24d516b1.css"
+  },
+  {
+    "revision": "bb2395e1ef162f4ae582",
+    "url": "/css/chunk-8b135ed2.d31440a8.css"
+  },
+  {
+    "revision": "e6e25c2edb5b79baeea4",
+    "url": "/css/compensate.e23d1a30.css"
+  },
+  {
+    "revision": "e0b267c185f193b522e8",
+    "url": "/css/income.fab68380.css"
+  },
+  {
+    "revision": "d06bf1a147facab03f3e",
+    "url": "/css/invitationList.5dc6dfe0.css"
+  },
+  {
+    "revision": "84db7e9089df55d6df83",
+    "url": "/css/join.f51ed62d.css"
+  },
+  {
+    "revision": "f185a3545819d07c955a",
+    "url": "/css/my.d66c2057.css"
+  },
+  {
+    "revision": "3b418bad1fbf4a5b9aa5",
+    "url": "/css/record.49c24154.css"
+  },
+  {
+    "revision": "1a27eaf21884eb7441abf2a5211596dc",
+    "url": "/favicon.png"
+  },
+  {
+    "revision": "10a09a2a28055789ffa497e1d4fae038",
+    "url": "/img/V0.10a09a2a.png"
+  },
+  {
+    "revision": "2ce79ac7712a8322b3ca224bfc6aeb42",
+    "url": "/img/V1.2ce79ac7.png"
+  },
+  {
+    "revision": "c78a0fb4da28ab9707c28da7a29b0304",
+    "url": "/img/V2.c78a0fb4.png"
+  },
+  {
+    "revision": "a1a30e617aca887abc5a9bfbf1b9c51b",
+    "url": "/img/V3.a1a30e61.png"
+  },
+  {
+    "revision": "cf51a0ebe19d4844f12efa51cbeb3f73",
+    "url": "/img/V4.cf51a0eb.png"
+  },
+  {
+    "revision": "010f543c6e0d7b1c64d43ffb989d2721",
+    "url": "/img/bitpie.010f543c.png"
+  },
+  {
+    "revision": "c402656c34c40d6c408a20990a096b73",
+    "url": "/img/first.c402656c.png"
+  },
+  {
+    "revision": "7a85dd8567cca7710016a1d8e877eaf8",
+    "url": "/img/home_bubble.7a85dd85.png"
+  },
+  {
+    "revision": "533b3e36806a1da1a65586673e781fdb",
+    "url": "/img/home_logo.533b3e36.png"
+  },
+  {
+    "revision": "f7acdd0916bd11c78db276cb9342c008",
+    "url": "/img/home_luckplayer.f7acdd09.png"
+  },
+  {
+    "revision": "d0ec5f09f790ee686fc5a20a8547aef7",
+    "url": "/img/income_logo.d0ec5f09.png"
+  },
+  {
+    "revision": "deeda4d7ca7df227a11bdea8e38d45fa",
+    "url": "/img/join_header.deeda4d7.png"
+  },
+  {
+    "revision": "efc990392e33c6cae6b2987eb7f1d3e5",
+    "url": "/img/join_limit.efc99039.png"
+  },
+  {
+    "revision": "9575a123a5e76f1ddcc1c4515fbf7067",
+    "url": "/img/not_wallet_logo.9575a123.png"
+  },
+  {
+    "revision": "982942377a33cc7b3c0300cb640139a9",
+    "url": "/img/notice_logo.98294237.png"
+  },
+  {
+    "revision": "3f865f934efebc55544c288e3f062846",
+    "url": "/img/redress_logo.3f865f93.png"
+  },
+  {
+    "revision": "09db897b74d557b08c0b6a9ab9ec20e2",
+    "url": "/img/second.09db897b.png"
+  },
+  {
+    "revision": "bd710ade41a50297d32a367877f479f3",
+    "url": "/img/swiper_item5.bd710ade.png"
+  },
+  {
+    "revision": "b0cf764085a4a87457db42793c60a3c2",
+    "url": "/img/third.b0cf7640.png"
+  },
+  {
+    "revision": "3155845225512433b4ac7c5a3b7b8b5c",
+    "url": "/img/top-title-bg.31558452.png"
+  },
+  {
+    "revision": "0542fe984fa3afdf58cea5f4ad600eaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82875b9691081b3453a2",
+    "url": "/js/againJoin.953e566e.js"
+  },
+  {
+    "revision": "e88b5d6e3fc1103b3a88",
+    "url": "/js/app.e99cdb36.js"
+  },
+  {
+    "revision": "9c03a7f3e9644dd40f10",
+    "url": "/js/chunk-2a69d8cb.2d12e3dc.js"
+  },
+  {
+    "revision": "c76a1610e4b450b2e2ae",
+    "url": "/js/chunk-54e934c9.550ea8dc.js"
+  },
+  {
+    "revision": "bb2395e1ef162f4ae582",
+    "url": "/js/chunk-8b135ed2.0b31398a.js"
+  },
+  {
+    "revision": "e2d2c71ee86230718e83",
+    "url": "/js/chunk-vendors.3c600be1.js"
+  },
+  {
+    "revision": "e6e25c2edb5b79baeea4",
+    "url": "/js/compensate.a11047bc.js"
+  },
+  {
+    "revision": "e0b267c185f193b522e8",
+    "url": "/js/income.9e2fc3cf.js"
+  },
+  {
+    "revision": "d06bf1a147facab03f3e",
+    "url": "/js/invitationList.ec711774.js"
+  },
+  {
+    "revision": "84db7e9089df55d6df83",
+    "url": "/js/join.8ae95a32.js"
+  },
+  {
+    "revision": "f185a3545819d07c955a",
+    "url": "/js/my.e122d078.js"
+  },
+  {
+    "revision": "3b418bad1fbf4a5b9aa5",
+    "url": "/js/record.fe2c059a.js"
+  },
+  {
+    "revision": "3d039d53e375abb98478cdf83925ab90",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "970996a24aa4d000ea56d3ac3d3a2763",
+    "url": "/static/en.html"
+  }
+]);
